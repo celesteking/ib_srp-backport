celesteking/ib_srp-backport

commit ecd7b7e9d82bbb78a3b8e9f569a9e9cd4daf4e79

IB/srp: Dynamically detect PD global_rkey support

drivers/infiniband/ulp/srp/ib_srp.c
@@ -4828,7 +4828,7 @@ static void srp_add_one(struct ib_device *device)
 		srp_dev->use_fmr = !srp_dev->use_fast_reg && srp_dev->has_fmr;
 	}
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+#if HAVE_PD_GLOBAL_RKEY
 	if (never_register || !register_always ||
 	    (!srp_dev->has_fmr && !srp_dev->has_fr))
 		flags |= IB_PD_UNSAFE_GLOBAL_RKEY;
@@ -4853,7 +4853,7 @@ static void srp_add_one(struct ib_device *device)
 	if (IS_ERR(srp_dev->pd))
 		goto free_dev;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0)
+#if !HAVE_PD_GLOBAL_RKEY
 	if (never_register || !register_always ||
 	    (!srp_dev->has_fmr && !srp_dev->has_fr) ||
 	    !HAVE_PD_LOCAL_DMA_LKEY) {
@@ -4883,7 +4883,7 @@ static void srp_add_one(struct ib_device *device)
 
 	goto free_attr;
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0)
+#if !HAVE_PD_GLOBAL_RKEY
 err_pd:
 	ib_dealloc_pd(srp_dev->pd);
 #endif

Makefile
@@ -242,6 +242,12 @@ conftest/ib_wr_reg_mr/result-$(KVER).txt: $(KERNEL_IMAGE)	\
 	conftest/ib_wr_reg_mr/Makefile
 	echo $(call run_conftest,ib_wr_reg_mr,-DHAVE_IB_WR_REG_MR) >$@
 
+conftest/pd_global_rkey/result-$(KVER).txt: $(KERNEL_IMAGE)	\
+	conftest/pd_global_rkey/pd_global_rkey.c		\
+	conftest/pd_global_rkey/Makefile
+	echo $(call run_conftest,pd_global_rkey,		\
+		-DHAVE_PD_GLOBAL_RKEY=1,-DHAVE_PD_GLOBAL_RKEY=0) >$@
+
 conftest/pd_local_dma_lkey/result-$(KVER).txt: $(KERNEL_IMAGE)	\
 	conftest/pd_local_dma_lkey/pd_local_dma_lkey.c		\
 	conftest/pd_local_dma_lkey/Makefile

conftest/pd_global_rkey/Makefile
@@ -1,0 +1,3 @@
+LINUXINCLUDE := $(PRE_CFLAGS) $(LINUXINCLUDE)
+
+obj-m += pd_global_rkey.o

conftest/pd_global_rkey/pd_global_rkey.c
@@ -1,0 +1,11 @@
+#include <linux/module.h>
+#include <rdma/ib_verbs.h>
+
+static int modinit(void)
+{
+	struct ib_pd pd = { .unsafe_global_rkey = 0x123456fe };
+
+	return pd.unsafe_global_rkey;
+}
+
+module_init(modinit);

drivers/infiniband/ulp/srp/ib_srp.h
@@ -116,7 +116,7 @@ struct srp_device {
 	struct list_head	dev_list;
 	struct ib_device       *dev;
 	struct ib_pd	       *pd;
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0)
+#if !HAVE_PD_GLOBAL_RKEY
 	struct ib_mr	       *global_mr;
 #endif
 	u32			global_rkey;

commit 1dea5c645420c19b8ba9ec171620579758516404

IB/srp: Fix RHEL/CentOS 7.4 build
Fixes: commit ecd7b7e9d82b ("IB/srp: Dynamically detect PD global_rkey support")
From: https://github.com/celesteking

drivers/infiniband/ulp/srp/ib_srp.c
@@ -5183,7 +5183,7 @@ static void srp_remove_one(struct ib_device *device, void *client_data)
 		kfree(host);
 	}
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 0)
+#if !HAVE_PD_GLOBAL_RKEY
 	if (srp_dev->global_mr)
 		ib_dereg_mr(srp_dev->global_mr);
 #endif